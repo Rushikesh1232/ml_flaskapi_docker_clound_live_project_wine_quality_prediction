15397c37750795ce151009c04bc611f078d54d60
Rushikesh1232 <waghchawarerushikesh@gmail.com>
2026-03-20 09:04:25 +0530

    updated app for railway deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import joblib
 import numpy as np
+import os 
 
 # Initialize app
 app = Flask(__name__)
@@ -30,5 +31,7 @@ def predict():
         return str(e)
 
 # Run app
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
